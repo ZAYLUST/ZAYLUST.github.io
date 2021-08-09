21844e319f7052c9578c3cf5fad6e3607b1ad661
happyguy12 <>
2021-08-09 21:11:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function bigImgTwitter(x) {
+x.style.height = "400px";
+x.style.width = "400px";
+}
+
+function normalImgTwitter(x) {
+x.style.height = "275px";
+x.style.width = "275px";
+}
+
+function bigImgInstagram(x) {
+x.style.height = "425px";
+x.style.width = "425px";
+}
+
+function normalImgInstagram(x) {
+x.style.height = "300px";
+x.style.width = "300px";
+}
+
+function bigImgWechat(x) {
+x.style.height = "375px";
+x.style.width = "375px";
+}
+
+function normalImgWechat(x) {
+x.style.height = "250px";
+x.style.width = "250px";
+}
